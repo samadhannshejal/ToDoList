49da034702fbfc9d96c0ce498c3fee09d062094e
samadhannshejal <samadhannshejal99@gmail.com>
2023-01-04 10:14:36 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const core = require('cors');
+const app = express();
+mongoose.set('strictQuery',false)
+app.use(core());
+app.use(express.json());
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("connected to DB")).catch(() => console.log("error"))
+const Todo = require('./Model/Todo')
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find()
+    res.json(todos)
+})
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    })
+    todo.save()
+    res.json(todo)
+})
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+
+    res.json({ result });
+});
+app.put('/todo/complete/:id',async(req,res)=>{
+    const todo=await Todo.findById(req.params.id);
+    todo.complete=!todo.complete;
+    todo.save()
+    res.json(todo)
+})
+
+app.listen(5000, () => console.log("server started on 5000 port"))
